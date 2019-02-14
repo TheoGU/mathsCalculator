b5b6df088523053ee4e21406b4b7f2986a2a4a4c
Guinebertier <theo@guinebertier.com>
2019-02-14 09:47:50 +0100

    Init drawer

--- exporapide/App.js
+++ exporapide/App.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Button,Text, View, StatusBar } from 'react-native';
+
+import { createDrawerNavigator, createAppContainer } from "react-navigation";
 
 import Calcul from './components/Calcul'
 import Tableau from './components/Tableau'
 import Setting from './components/Setting'
 
-export default class App extends React.Component {
+class Home extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Exponetiel Rapide',
+  };
   render() {
     return (
       <View style={styles.container}>
-      <StatusBar hidden />
+        <StatusBar hidden />
         <Calcul />
-        <Setting />
       </View>
     );
   }
 }
+
+class Pgcd extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Pgcd',
+  };
+
+  render() {
+    return (
+      <Text>hello</Text>
+    );
+  }
+}
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: Home,
+  },
+  Notifications: {
+    screen: Pgcd,
+  },
+});
+
+const App = createAppContainer(MyDrawerNavigator);
+
+
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 1, paddingTop: 5, backgroundColor: 'red' },
   head: { height: 40, backgroundColor: '#f1f8ff' },
   text: { margin: 6 }
 });
+
+
+export default App
